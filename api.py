1ba326055536934f253c20825aacbf0bf22ade14
aure-nogueras <anogueras@correo.ugr.es>
2020-10-09 13:34:10 +0200

    Añadido despliegue flask inicial #2, primer refrán y conexión con heroku

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Refran:
+	def get_refran(self, filtro):
+		return refranes[random.randint(0,len(refranes)-1]
+	
+	def generate_refran(self):
+		refran = get_refran(json.load(open('refranes.json')))
+		
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)

e72949bd684e7d945ce00403a8f40cf2a66e4a70
aure-nogueras <anogueras@correo.ugr.es>
2021-10-12 01:19:06 +0200

    Añade botón para generar refranes #4 y #5

--- webapp.py
+++ webapp.py
@@ -1,11 +1,14 @@
-from flask import Flask, redirect, url_for, render_template, current_app
+from flask import Flask, redirect, url_for, render_template, current_app, request
 from api import Refran
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
 	refran = Refran()
+	if request.method == 'POST':
+		return render_template('index.html', linea=refran.generate_refran())
+		
 	return render_template('index.html', linea=refran.generate_refran())
     
 if __name__ == '__main__':
